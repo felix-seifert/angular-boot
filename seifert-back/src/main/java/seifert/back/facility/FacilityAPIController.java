16206367afa74f176aa707898e3577015603c1ce
felix-seifert <seifert.felix@gmx.net>
2018-08-17 23:46:26 +0200

    Add dependency for CORS
    * Allow the localhost with port 4200 To access the facility APIs

--- seifert-back/src/main/java/seifert/back/facility/FacilityAPIController.java
+++ seifert-back/src/main/java/seifert/back/facility/FacilityAPIController.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/facilities")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FacilityAPIController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FacilityAPIController.class);
